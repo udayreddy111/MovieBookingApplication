1b0fddc4a64050b6002aa92d4ad1681daef16295
INE12393403 <m.uday@tesco.com>
2025-12-17 21:23:46 +0530

    the is the first commit

---
+++ MovieRegistration/MovieRegistration/src/test/java/com/jaswin/MovieRegistration/MovieRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaswin.MovieRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
