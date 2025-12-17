87a733af72b4d736ee32c51632b7a4200b97bef1
Nitheesh <nitheeshkumar@gmail.com>
2025-12-17 21:57:31 +0530

    added Post API

--- MovieRegistration/MovieRegistration/src/main/java/com/jaswin/MovieRegistration/controller/MovieRegistrationController.java
+++ MovieRegistration/MovieRegistration/src/main/java/com/jaswin/MovieRegistration/controller/MovieRegistrationController.java
@@ -1,6 +1,7 @@
 package com.jaswin.MovieRegistration.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,11 @@ public class MovieRegistrationController {
     @GetMapping
     String testMessage() {
         return "hi iam working fine";
+        
+    }
+    @PostMapping
+    String testMessage1() {
+        return "hi iam working fine";
+        
     }
 }
