6687e7fedd8f0914ec3f1893f73770e5a215c274
Sivanandanraju88 <sivanandanraju88@gmail.com>
2025-12-18 12:22:46 +0530

    Added Theatre entty

---
+++ TheaterRegistration/src/test/java/com/jaswin/TheaterRegistration/TheaterRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaswin.TheaterRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
