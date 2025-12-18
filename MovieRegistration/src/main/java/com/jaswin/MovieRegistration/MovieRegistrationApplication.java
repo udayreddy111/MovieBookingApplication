fe0bfd85a5cabb493a0ba13d1d9ea4996ea04f1c
INE12393403 <m.uday@tesco.com>
2025-12-18 22:54:07 +0530

    Modify the code structure

--- MovieRegistration/src/main/java/com/jaswin/MovieRegistration/MovieRegistrationApplication.java
+++ MovieRegistration/src/main/java/com/jaswin/MovieRegistration/MovieRegistrationApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieRegistrationApplication {
 
+
 	public static void main(String[] args) {
+
+		System.out.println("new branch craeted");
 		SpringApplication.run(MovieRegistrationApplication.class, args);
 	}
 
