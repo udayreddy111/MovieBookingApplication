825ee2e1aca491392c33d24dee4f6d43fdb84c80
Charan3836 <saicharan10784@gmail.com>
2025-12-18 19:54:40 +0530

    Added repository later , service layer modified entity class and controlller

--- MovieRegistration/MovieRegistration/src/main/java/com/jaswin/MovieRegistration/entity/MovieBooking.java
+++ MovieRegistration/MovieRegistration/src/main/java/com/jaswin/MovieRegistration/entity/MovieBooking.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,13 +23,14 @@ public class MovieBooking {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long theaterid;
+	@NotBlank(message = "movie name must not be empty")
 	
 	private String movienames;
 	
 	private LocalDate moviedate;
 	
 	private String language;
-	
+	@NotNull(message ="moviestarttime must not be null")
 	private LocalTime moviestarttime;
 	
 	private int price;
